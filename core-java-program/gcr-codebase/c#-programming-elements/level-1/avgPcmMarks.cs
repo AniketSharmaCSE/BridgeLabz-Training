116aa6ab22e2b4e0c4f94cb9b3b4856216114f20
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-20 12:32:55 +0530

    [Aniket] Add. Calculating average marks of a student

---
+++ core-java-program/gcr-codebase/c#-programming-elements/level-1/avgPcmMarks.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class avgPcmMarks{
+	static void Main(){
+		int maths = 94;
+		int physics = 95;
+		int chemistry = 96;
+		int avgMarks = (maths + physics + chemistry)/3;
+		Console.WriteLine("Sam;s average mark in PCM: " + avgMarks);
+	}
+}
