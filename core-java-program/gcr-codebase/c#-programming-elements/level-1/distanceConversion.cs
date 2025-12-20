935618351af277e38a940d8cae5cc3541d82edf3
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-20 12:37:03 +0530

    [Aniket] Add. Converting distance from kilometres to miles

---
+++ core-java-program/gcr-codebase/c#-programming-elements/level-1/distanceConversion.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class distanceConversion{
+	static void Main(){
+		double distanceInKm = 10.8;
+		double distanceInMiles = distanceInKm*1.6;
+		Console.WriteLine("The Distance 10.8 km in Miles is " + distanceInMiles);
+	}
+}
