265472b49562f026819eac102405ea3c1aa0c1f6
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-22 16:13:49 +0530

    [Aniket] Add. Checking if user is eligible to vote or not

---
+++ core-java-program/gcr-codebase/control-flow/level-1/VoteEligibility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class VoteEligibility
+{
+	static void Main(string[] args)
+	{
+		//Ask user for their age
+		Console.WriteLine("Enter your age: ");
+		int age = Convert.ToInt32(Console.ReadLine());
+
+		//Check if above or equal to 18
+		if (age >= 18)
+		{
+			//print if eligible
+			Console.WriteLine("The person's age is: " + age + " and can vote");
+		}
+		else {
+			//print if not eligible
+            Console.WriteLine("The person's age is: " + age + " and cannot vote");
+        }
+	}
+}
