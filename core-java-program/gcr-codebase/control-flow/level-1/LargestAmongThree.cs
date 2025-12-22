2f00386a7952486400b73a45b974ac14ec9b6d88
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-22 15:41:01 +0530

    [Aniket] Add. Finding largest number among three numbers

---
+++ core-java-program/gcr-codebase/control-flow/level-1/LargestAmongThree.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class LargestAmongThree
+{
+    static void Main(string[] args)
+    {
+        //Take input for all three numbers
+        Console.WriteLine("Enter first number: ");
+        double first = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter second number: ");
+        double second = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter third number: ");
+        double third = Convert.ToDouble(Console.ReadLine());
+
+        //Variable to store largest number
+        double largest = 0;
+
+        //Conditions to find largest among three
+        if (first > second && first > third)
+        {
+            largest = first;
+        }
+        else if (second > third && second > first)
+        {
+            largest = second;
+
+        }
+        else
+        {
+            largest = third;
+        }
+
+        //print the largest number 
+        Console.WriteLine("The largest number is: " + largest);
+    }
+}
