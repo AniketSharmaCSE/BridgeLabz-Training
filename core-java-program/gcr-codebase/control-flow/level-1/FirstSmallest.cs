5a2e7a52fd6ee98204ae1ba438d04d1397205942
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-22 15:26:33 +0530

    [Aniket] Add. Checking if fist number is smaller than other two

---
+++ core-java-program/gcr-codebase/control-flow/level-1/FirstSmallest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class FirstSmallest
+{
+    static void Main(string[] args)
+    {
+        //Take input for all three numbers
+        Console.WriteLine("Enter first number: ");
+        double first = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter second number: ");
+        double second = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter third number: ");
+        double third = Convert.ToDouble(Console.ReadLine());
+
+        if (first < second && first < third)
+        {
+            Console.WriteLine("First is the smallest number");
+
+        }
+        else
+        {
+            Console.WriteLine("First is not the smallest");
+        }
+    }
+}
