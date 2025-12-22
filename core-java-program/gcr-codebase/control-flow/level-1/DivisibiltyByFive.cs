a6669e27e1ed88e7715f0ddf4e313823e2cf8032
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-22 15:17:52 +0530

    [Aniket] Add. Program to check divisibilty with 5

---
+++ core-java-program/gcr-codebase/control-flow/level-1/DivisibiltyByFive.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class DivisilityByFive
+{
+	static void Main(string[] args)
+	{
+		//ask user for a number
+		Console.WriteLine("Enter a number: ");
+		double num = Convert.ToDouble(Console.ReadLine());
+
+		//Check if divisible by 5
+		if (num % 5 == 0)
+		{
+			//print is divisible
+			Console.WriteLine(num + " is divisible by 5");
+		}
+			//print not divisible
+		else {
+            Console.WriteLine(num + " is not divisible by 5");
+        }
+	}
+}
