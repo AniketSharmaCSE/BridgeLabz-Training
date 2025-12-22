7c0a53bc48f9a36ff9f184b1c5426acde765cb2f
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-22 16:25:20 +0530

    [Aniket] Add. Checking if number is positive, negative or zero

---
+++ core-java-program/gcr-codebase/control-flow/level-1/SignOfNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class SignOfNumber
+{
+	static void Main(string[] args)
+	{
+		//Accept a number from user(
+		Console.WriteLine("Enter a number: ");
+		double number = Convert.ToDouble(Console.ReadLine());
+
+		//Check if positive
+		if (number > 0)
+		{
+			Console.WriteLine("Positive");
+		}
+		//check if negative
+		else if (number < 0)
+		{
+			Console.WriteLine("Negative");
+		}
+		//print zero when neither positive nor negative
+		else
+		{
+			Console.WriteLine("zero");
+		}
+	}
+}
