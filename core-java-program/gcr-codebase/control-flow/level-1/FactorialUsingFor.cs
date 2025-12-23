a4fd791f0639460e612da64e96567f4a065f360e
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-23 14:40:08 +0530

    [Aniket] Add. Calculating factorial of a given number using for loop

---
+++ core-java-program/gcr-codebase/control-flow/level-1/FactorialUsingFor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Numerics;
+
+public class FactorialUsingFor
+{
+    static void Main(string[] args)
+    {
+        //input a positive number
+        Console.WriteLine("Enter a positive number: ");
+        int input = Convert.ToInt32(Console.ReadLine());
+
+        //check if positive
+        if (input <= 0)
+        {
+            Console.WriteLine("Number is not positive");
+            return;
+        }
+
+        //data type big integer because it can handle larger numbers 
+        //Int data type contains only 32 or 64 bits, which fails for larger number
+        BigInteger factorial = 1;
+
+        //While loop to calculate factorial
+        for (int i = 1; i <= input; i++)
+        {
+            factorial *= i;
+        }
+        //print the factorial
+        Console.WriteLine(factorial);
+    }
+}
