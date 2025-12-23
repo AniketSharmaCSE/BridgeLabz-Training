c9f625f2a0a7cd66dfe1bfd7bf928a9163475d6a
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-23 12:21:02 +0530

    [Aniket] Add. Finding and comparing sum of n natural numbers using formula and for loop

---
+++ core-java-program/gcr-codebase/control-flow/level-1/SumCompareUsingFor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class SumCompareUsingFor
+{
+    static void Main(string[] args)
+    {
+        //ask user for a number
+        Console.WriteLine("Enter a natural number: ");
+        int input = Convert.ToInt32(Console.ReadLine());
+
+        //variable for storing sum
+        int TotalFormula = 0;
+        int TotalFor = 0;
+
+        //Check if number is natural number
+        if (input <= 0)
+        {
+            Console.WriteLine(input + " is not a natural number");
+        }
+        else
+        {
+
+            TotalFormula = input * (input + 1) / 2;
+
+            for (int i = 1; i <= input; i++)
+            {
+                TotalFor += i;
+            }
+        }
+        //Print and compare result
+        Console.WriteLine("Sum using formula is: " + TotalFormula);
+        Console.WriteLine("Sum using for loop is: " + TotalFor);
+        Console.WriteLine("Are they both equal? " + (TotalFor == TotalFormula));
+    }
+}
