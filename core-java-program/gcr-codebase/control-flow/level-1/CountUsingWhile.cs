60b1dea317f8233c8d1421ae612d28c868fe0502
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-23 11:04:12 +0530

    [Aniket] Add. Counting down for a rocket launch using while loop

---
+++ core-java-program/gcr-codebase/control-flow/level-1/CountUsingWhile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class CountUsingWhile
+{
+	static void Main(string[] args)
+	{
+		//Ask user for a number
+		Console.WriteLine("Enter the start of the count down: ");
+		
+		int start = Convert.ToInt32(Console.ReadLine());
+
+		//Create while loop for count down
+		while (start != 1) {
+			
+			//print the number
+			Console.WriteLine(start);
+            //decrement the number
+            start--;
+        }
+	}
+}
