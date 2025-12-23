abeecd4fdc3c5e1a79c1364f5fa0d2e253b02d06
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-23 11:54:27 +0530

    [Aniket] Add. Adding user input values until 0 or negative number is entered

---
+++ core-java-program/gcr-codebase/control-flow/level-1/SumWithBreak.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class SumWithBreak
+{
+	static void Main(string[] args)
+	{	
+		//Declare and initialies variables to store sum and input value
+		double total = 0;
+		double input = 1;
+
+		while (true)
+		{
+			//ask user for input
+			Console.WriteLine("Enter a number to add");
+			Console.WriteLine("Enter zero or negative number to exit: ");
+			input = Convert.ToDouble(Console.ReadLine());
+
+			//Check and break loop if input 0 or negative
+			if (input == 0 || input < 0)
+			{
+				break;
+			}
+			total += input;
+
+		}
+		//print the total
+		Console.WriteLine("The total is: " + total);
+	}
+}
+
