3d4b8873d08c2a72c4333c26c9701a8fcb395b0a
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-23 11:13:23 +0530

    [Aniket] Add. Counting down for a rocket launch using for loop

---
+++ core-java-program/gcr-codebase/control-flow/level-1/CountUsingFor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class CountUsingFor
+{
+    static void Main(string[] args)
+    {
+        //Ask user for a number
+        Console.WriteLine("Enter the start of the count down: ");
+
+        int start = Convert.ToInt32(Console.ReadLine());
+
+        //Create while loop for count down
+        for(int i = start; i>=1; i--)
+        {
+
+            //print the number
+            Console.WriteLine(i);
+        }
+    }
+}
