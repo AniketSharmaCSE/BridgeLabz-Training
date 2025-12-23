3455e25d286b0d715cd16cc18ad792a4bc3917b4
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-23 15:11:01 +0530

    [Aniket] Add. Printing all the even and odd numbers upto user entered number

---
+++ core-java-program/gcr-codebase/control-flow/level-1/EvenOdd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class EvenOdd
+{
+	static void Main(string[] args)
+	{
+		//ask user for end point
+		Console.WriteLine("Enter a number: ");
+
+		int number = Convert.ToInt32(Console.ReadLine());
+
+		//loop from 0 to input number
+		for (int i = 0; i <= number; i++)
+		{
+			//check if even
+			if (i % 2 == 0)
+			{
+				Console.WriteLine(i + " even");
+			}
+			else
+			{
+				//if not even, then odd
+				Console.WriteLine(i + " odd");
+			}
+		}
+	}
+}
