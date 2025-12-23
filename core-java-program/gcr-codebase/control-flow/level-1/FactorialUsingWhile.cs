e65d434b9d6ead522d822bcc17f6a3ba561f1f3f
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-23 12:43:48 +0530

    [Aniket] Add. Calculating factorial of a given number using while loop

---
+++ core-java-program/gcr-codebase/control-flow/level-1/FactorialUsingWhile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Numerics;
+
+public class FactorialUsingWhile
+{
+	static void Main(string[] args)
+	{
+		//input a positive number
+		Console.WriteLine("Enter a positive number: ");
+		int input = Convert.ToInt32(Console.ReadLine());
+
+		//check if positive
+		if (input <= 0) {
+			Console.WriteLine("Number is not positive");
+			return;		
+		}
+
+		//data type big integer because it can handle larger numbers 
+		//Int data type contains only 32 or 64 bits, which fails for larger number
+		BigInteger factorial = 1;
+
+		//While loop to calculate factorial
+		while (input > 0) { 
+			factorial *= input;
+			input--;
+		}
+		//print the factorial
+		Console.WriteLine(factorial);
+	}
+}
