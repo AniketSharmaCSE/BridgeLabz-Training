e604632bbfaf728e9160fcf460060019c48e9d34
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-24 09:51:16 +0530

    [Aniket] Add. Checking if given year is leap year or not

---
+++ core-java-program/gcr-codebase/control-flow/level-2/LeapYear.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class LeapYear
+{
+    static void Main(string[] args)
+    {
+        //Ask user to enter a year
+        Console.WriteLine("Enter a year: ");
+        int year = Convert.ToInt32(Console.ReadLine());
+
+
+
+            //Check leap year conditions
+            if (year % 400 == 0)
+            {
+                Console.WriteLine(year + " is a Leap Year");
+            }
+            else if (year % 100 == 0)
+            {
+                Console.WriteLine(year + " is not a Leap Year");
+            }
+            else if (year % 4 == 0)
+            {
+                Console.WriteLine(year + " is a Leap Year");
+            }
+            else
+            {
+                Console.WriteLine(year + " is not a Leap Year");
+            }
+
+    }
+}
