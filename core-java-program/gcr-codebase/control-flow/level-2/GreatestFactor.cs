48c91fc89cc4f91b80097467135bc43aed0b484d
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-24 10:18:33 +0530

    [Aniket] Add. Program to find the greatest factor of a given number using loop

---
+++ core-java-program/gcr-codebase/control-flow/level-2/GreatestFactor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class GreatestFactor
+{
+    static void Main(string[] args)
+    {
+        //Take input from user
+        Console.WriteLine("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        //initialise variable with greatest factor 1
+        int greatestFactor = 1;
+
+        //Loop from number-1 to 1
+        for (int i = number - 1; i >= 1; i--){
+            if (number % i == 0){
+                greatestFactor = i;
+                break;
+            }
+        }
+
+
+        //print the greatest factor
+        Console.WriteLine("Greatest factor is: " + greatestFactor);
+    }
+}
