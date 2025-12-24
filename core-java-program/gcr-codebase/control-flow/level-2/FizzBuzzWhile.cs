8724af4fc9a17b079d95ac75e664a7eaa28d636d
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-24 10:02:02 +0530

    [Aniket] Add. Implemented FizzBuzz Program using While loop

---
+++ core-java-program/gcr-codebase/control-flow/level-2/FizzBuzzWhile.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class FizzBuzzWhile
+{
+    static void Main(string[] args)
+    {
+        //Ask user for a end point
+        Console.WriteLine("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int i = 1;
+
+
+        //While loop to traverse till end point
+        while (i <= number)
+        {
+            //Condition to check where to print 
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+
+            i++;
+        }
+    }
+}
