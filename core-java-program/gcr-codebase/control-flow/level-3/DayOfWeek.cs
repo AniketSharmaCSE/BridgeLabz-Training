6a841342e44dc7d42405d121984a9344af0f14e7
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-24 10:54:21 +0530

    [Aniket] Add. Finding day of week when date, month and year is given by user

---
+++ core-java-program/gcr-codebase/control-flow/level-3/DayOfWeek.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class DayOfWeek
+{
+    static void Main(string[] args)
+    {
+        //Ask user for month, day and year
+        Console.WriteLine("Enter month: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter day: ");
+        int d = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter year: ");
+        int y = Convert.ToInt32(Console.ReadLine());
+
+
+        //Applying given formula
+        int y0 = y - (14 - m) / 12;
+        int x = y0 + y0 / 4 - y0 / 100 + y0 / 400;
+        int m0 = m + 12 * ((14 - m) / 12) - 2;
+        int d0 = (d + x + (31 * m0) / 12) % 7;
+
+
+        //print the result
+        Console.WriteLine("Day of week is: " + d0);
+    }
+}
