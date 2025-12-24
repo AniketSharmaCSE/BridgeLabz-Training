256a32c7ba862f38b41e75bcc91a25d473186fc9
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-24 10:50:56 +0530

    [Aniket] Add. Program to check if a number is abundant number or not

---
+++ core-java-program/gcr-codebase/control-flow/level-3/AbundantNumber.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class AbundantNumber
+{
+    static void Main(string[] args)
+    {
+        //Input a number from user
+        Console.WriteLine("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        //Variable to store sum
+        int sum = 0;
+
+        //Find sum of divisors
+        for (int i = 1; i < number; i++){
+            if (number % i == 0){
+                sum = sum + i;
+            }
+        }
+        //Check if abundant number 
+        if (sum > number){
+            Console.WriteLine(number + " is an Abundant Number");
+        }
+        else{
+            Console.WriteLine(number + " is not an Abundant Number");
+        }
+    }
+}
