72ec0a2a3df9dc872c1b1553b1859653da67961b
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-24 10:04:30 +0530

    [Aniket] Add. Program to find the BMI of a person

---
+++ core-java-program/gcr-codebase/control-flow/level-2/BMI.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class BMI
+{
+    static void Main(string[] args)
+    {
+        //Ask user for their weight and height
+        Console.WriteLine("Enter weight in kg: ");
+        double weight = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter height in cm: ");
+        double heightCm = Convert.ToDouble(Console.ReadLine());
+
+
+        //Convert height to meters
+        double height = heightCm / 100;
+
+        //Calculate and print BMI
+        double bmi = weight / (height * height);
+        Console.WriteLine("BMI value is: " + bmi);
+    }
+}
