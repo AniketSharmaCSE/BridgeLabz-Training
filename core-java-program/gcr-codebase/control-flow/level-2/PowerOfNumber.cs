c5b3f95f8248c5e978cc116b6e6afd75d3c13356
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-24 10:21:40 +0530

    [Aniket] Add. Calculating power of a given number using loops

---
+++ core-java-program/gcr-codebase/control-flow/level-2/PowerOfNumber.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class PowerOfNumber
+{
+    static void Main(string[] args)
+    {
+        //Ask user for number and power
+        Console.WriteLine("Enter number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter power: ");
+        int power = Convert.ToInt32(Console.ReadLine());
+
+        int result = 1;
+
+        //For loop to calculate power
+        for (int i = 1; i <= power; i++){
+            result = result * number;
+        }
+
+        //print the result
+        Console.WriteLine("Result is: " + result);
+    }
+}
