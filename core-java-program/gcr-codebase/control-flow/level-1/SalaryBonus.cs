985b80a7f34a47d47da8c9b7245db9ffee11f14e
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-24 09:37:17 +0530

    [Aniket] Add. Checking and printing bonus for employees with more than 5 years of service

---
+++ core-java-program/gcr-codebase/control-flow/level-1/SalaryBonus.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class SalaryBonus
+{
+	static void Main(string[] args)
+	{
+		//Input salary and years of service
+		Console.WriteLine("Enter your Salary: ");
+		double salary = Convert.ToDouble(Console.ReadLine());
+		Console.WriteLine("Enter years of service: ");
+        double years = Convert.ToDouble(Console.ReadLine());
+
+		//Check if years less than five
+		if (years < 5)
+		{
+			Console.WriteLine("Not eligible for bonus!");
+		}
+		else {
+			//Calculate and print bonus amount
+			double bonus = salary / 100 * 5;
+			Console.WriteLine("Your bonus amount is: " + bonus);
+		}
+    }
+}
