bad09d1ad638eacf23c62428113d6f1901986a7d
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-24 10:42:04 +0530

    [Aniket] Add. Program to count digits in a given integer

---
+++ core-java-program/gcr-codebase/control-flow/level-3/DigitCount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class DigitCount
+{
+    static void Main(string[] args)
+    {
+        //Ask user for the number
+        Console.WriteLine("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        //initialize count variable
+        int count = 0;
+
+        //Count digits using while loop
+        while (number != 0){
+            number = number / 10;
+            count++;
+        }
+
+        Console.WriteLine("Total digits: " + count);
+    }
+}
