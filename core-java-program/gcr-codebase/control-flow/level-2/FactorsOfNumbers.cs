d2a7684b06750b56191e080783038b4e91192399
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-24 10:24:57 +0530

    [Aniket] Add. Finding factors of a number using for loop

---
+++ core-java-program/gcr-codebase/control-flow/level-2/FactorsOfNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class FactorsOfNumber
+{
+    static void Main(string[] args)
+    {
+        //Ask user for a number
+        Console.WriteLine("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+
+        //Check factors of the numbers using for loop
+        for (int i = 1; i < number; i++){
+            if (number % i == 0){
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
