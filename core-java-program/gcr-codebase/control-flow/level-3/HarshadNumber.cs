a712b9e55685f6162cc3ef67f3328f70f0a5373b
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-24 10:47:17 +0530

    [Aniket] Add. Program to check if number given by user is harshad nnumber or not

---
+++ core-java-program/gcr-codebase/control-flow/level-3/HarshadNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class HarshadNumber
+{
+    static void Main(string[] args)
+    {
+        //Ask user for number
+        Console.WriteLine("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        //Variable to store sum and temporary value
+        int sum = 0;
+        int temp = number;
+
+        //Calculate sum of digits
+        while (temp != 0){
+            int digit = temp % 10;
+            sum = sum + digit;
+            temp = temp / 10;
+        }
+
+        //Check Harshad condition
+        if (number % sum == 0{
+            Console.WriteLine(number + " is a Harshad Number");
+        }
+        else{
+            Console.WriteLine(number + " is not a Harshad Number");
+        }
+    }
+}
