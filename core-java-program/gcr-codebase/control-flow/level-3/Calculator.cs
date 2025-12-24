322fa847e9eecbb26d403ca68bb4e7bc809d6a1a
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-24 10:56:02 +0530

    [Aniket] Add. Calculator to perform operation between two operands using switch case

---
+++ core-java-program/gcr-codebase/control-flow/level-3/Calculator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class Calculator
+{
+    static void Main(string[] args)
+    {
+        //Ask user for operands
+        Console.WriteLine("Enter first number: ");
+        double first = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter second number: ");
+        double second = Convert.ToDouble(Console.ReadLine());
+
+        //input the operator
+        Console.WriteLine("Enter operator (+, -, *, /): ");
+        string op = Console.ReadLine();
+
+
+        //Perform operation using switch
+        switch (op)
+        {
+            case "+":
+                Console.WriteLine("Result: " + (first + second));
+                break;
+
+            case "-":
+                Console.WriteLine("Result: " + (first - second));
+                break;
+
+            case "*":
+                Console.WriteLine("Result: " + (first * second));
+                break;
+
+            case "/":
+                Console.WriteLine("Result: " + (first / second));
+                break;
+
+            default:
+                Console.WriteLine("Invalid Operator");
+                break;
+        }
+    }
+}
