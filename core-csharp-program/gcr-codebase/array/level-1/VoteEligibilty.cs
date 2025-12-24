6e02cfec1edc6d4d484493f01ee0d4d7e5bcc3da
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-24 15:36:44 +0530

    [Aniket] Add. Storing age of students in array and checking voting eligibility

---
+++ core-csharp-program/gcr-codebase/array/level-1/VoteEligibilty.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class VoteEligibilty
+{
+    static void Main(string[] args)
+    {
+		//Initialise an array of length 10
+        int[] age = new int[10];
+		
+		//For loop to input values for the array
+        for (int i = 0; i < age.Length; i++)
+        {
+            Console.WriteLine(i + ". Enter age: ");
+
+            age[i] = Convert.ToInt32(Console.ReadLine());
+        }
+		//for loop to traverse through array and check if eligible or not
+        for (int j = 0; j < age.Length; j++)
+        {
+            if (age[j] <= 0)
+            {
+                Console.WriteLine("Enter a positive age!");
+            }
+            else if (age[j] < 18)
+            {
+                Console.WriteLine("The student with age " + age[j] + " cannot vote");
+            }
+            else
+            {
+                Console.WriteLine("The student with age " + age[j] + " can vote");
+            }
+        }
+    }
+}
