26bebc31facdafb47805757396dfd82f750d6184
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2025-12-28 22:14:08 +0530

    [Aniket] Add. Program to comapare to strings using built-in and user defined method

---
+++ core-csharp-program/gcr-codebase/csharp-strings/CompareString.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class CompareString
+{
+    static bool compare(string a, string b)
+    {
+        if (a.Length != b.Length)
+        {
+            return false;
+        }
+        else
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] != b[i])
+                {
+
+                    return false;
+
+                }
+            }
+            return true;
+
+        }
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            //Take input for both strings
+            Console.WriteLine("Enter string 1: ");
+            string first = Console.ReadLine();
+            Console.WriteLine("Enter string 2: ");
+            string second = Console.ReadLine();
+
+            //Compare using user-defined function
+            if (compare(first, second))
+            {
+                Console.WriteLine("Both are equal");
+            }
+
+        }
+
+    }
+}
