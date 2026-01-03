67902178d017f79ea8a8f3adee852b5c2e345e00
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2026-01-03 15:21:43 +0530

    [Aniket] Add. Program to implement class and objects to store and display employee details

---
+++ oops-csharp-practice/gcr-codebase/csharp-class-object/employee-details/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Employee
+{
+    
+    public string name;
+    public int id;
+    public double salary;
+
+
+    // Constructor to initialize employee data at object creation
+    public Employee(string name, int id, double salary)
+    {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    
+    // Method to display employee details
+    public void DisplayDetails()
+    {
+        Console.WriteLine("Employee Name: " + name);
+        Console.WriteLine("Employee ID: " + id);
+        Console.WriteLine("Employee Salary: " + salary);
+    
+    }
+}
---
+++ oops-csharp-practice/gcr-codebase/csharp-class-object/employee-details/EmployeeDetails.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class EmployeeDetails
+{
+    static void Main(string[] args)
+    {
+        // Taking input from user before creating object
+        Console.WriteLine("Enter employee name:");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Enter employee id:");
+        int id = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter employee salary:");
+        double salary = Convert.ToDouble(Console.ReadLine());
+
+        // Creating object using constructor
+        Employee emp = new Employee(name, id, salary);
+
+        Console.WriteLine();
+        emp.DisplayDetails();
+    }
+}
