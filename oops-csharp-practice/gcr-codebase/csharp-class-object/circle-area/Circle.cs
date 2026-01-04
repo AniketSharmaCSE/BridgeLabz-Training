7da7d0eb1c803b163f5515ceb39e120b12fe6b2c
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2026-01-04 20:43:53 +0530

    [Aniket] Add. Circle class with attribute radius and method to calculate area

---
+++ oops-csharp-practice/gcr-codebase/csharp-class-object/circle-area/Circle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Circle
+{
+    double radius;
+    //Constructor to initialize radius
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    //calculate area of circle
+    public double CalculateArea()
+    {
+        return 3.14 * radius * radius;
+    }
+
+
+    //calculate circumference of circle
+    public double CalculateCircumference()
+    {
+      
+        return 2 * 3.14 * radius;
+    }
+
+
+    //display results
+    public void DisplayDetails()
+    {
+        Console.WriteLine("Radius: " + radius);
+        Console.WriteLine("Area of Circle: " + CalculateArea());
+        Console.WriteLine("Circumference of Circle: " + CalculateCircumference());
+    }
+}
---
+++ oops-csharp-practice/gcr-codebase/csharp-class-object/circle-area/CircleDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class CircleDetails
+{
+    static void Main(string[] args)
+    {
+        //Taking radius input from user
+        Console.WriteLine("Enter radius of the circle:");
+        double radius = Convert.ToDouble(Console.ReadLine());
+        //Creating object using constructor
+        Circle circle = new Circle(radius);
+
+        Console.WriteLine();
+        circle.DisplayDetails();
+    }
+}
