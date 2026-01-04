d266237e8cfca0db0fc41e1cd1ca5bec3367f16b
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2026-01-04 21:45:56 +0530

    [Aniket] Add. Book class with defualt and paramaterized constructors

---
+++ oops-csharp-practice/gcr-codebase/csharp-constructors/book-details/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Book
+{
+    string title;
+    string author;
+    double price;
+    
+
+
+    //Default constructor
+    public Book()
+    {
+        title = "Not Assigned";
+        author = "Unknown";
+        price = 0;
+    }
+
+
+    //Parameterized constructor
+    public Book(string title, string author, double price)
+    {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+
+    //Method to display book details
+    public void DisplayDetails()
+    {
+        Console.WriteLine(title + " " + author + " " + price);
+    }
+}
---
+++ oops-csharp-practice/gcr-codebase/csharp-constructors/book-details/BookDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class BookDetails
+{
+    static void Main(string[] args)
+    {
+        //Creating object using default constructor
+        Book b1 = new Book();
+
+        //Creating object using parameterized constructor
+        Book b2 = new Book("CSharp Basics", "Microsoft", 500);
+
+        b1.DisplayDetails();
+        b2.DisplayDetails();
+    }
+}
