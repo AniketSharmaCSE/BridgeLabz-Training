8987b0e0e42e1ffb181728022439d8771c2495d9
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2026-01-04 20:55:44 +0530

    [Aniket] Add. Bookk class with details of books and method to book details

---
+++ oops-csharp-practice/gcr-codebase/csharp-class-object/book-details/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Book
+{
+    string title;
+    string author;
+    double price;
+
+   
+    //Constructor to initialize book details
+    public Book(string title, string author, double price)
+    {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    
+   //Method to display book details
+    public void DisplayDetails()
+    {
+        Console.WriteLine("Title: " + title);
+        Console.WriteLine("Author: " + author);
+        Console.WriteLine("Price:" + price);
+    }
+}
---
+++ oops-csharp-practice/gcr-codebase/csharp-class-object/book-details/BookDetails.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class BookDetails
+{
+    static void Main(string[] args)
+    {
+
+        //Taking input from user
+        Console.WriteLine("Enter book title:");
+        string title = Console.ReadLine()
+        Console.WriteLine("Enter author name:");
+        string author = Console.ReadLine()
+        Console.WriteLine("Enter book price:");
+        double price = Convert.ToDouble(Console.ReadLine());
+
+      
+        Book book = new Book(title, author, price
+        book.DisplayDetails();
+    }
+}
