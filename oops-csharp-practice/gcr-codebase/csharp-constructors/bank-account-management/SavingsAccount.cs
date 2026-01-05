fa9cc04124e2ae04365e136b98e33035af4e8b2e
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2026-01-05 15:43:11 +0530

    [Aniket] Add. Bank account management system demonstrating access modifiers

---
+++ oops-csharp-practice/gcr-codebase/csharp-constructors/bank-account-management/SavingsAccount.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class SavingsAccount : BankAccount
+{
+    //Constructor calling base class constructor
+    public SavingsAccount(int accountNumber, string accountHolder, double balance) : base(accountNumber, accountHolder, balance)
+    {
+		
+    }
+
+    
+	//Method to access public and protected members
+    public void DisplayAccountInfo()
+    {
+        Console.WriteLine("Account Number: " + accountNumber);
+        Console.WriteLine("Account Holder: " + accountHolder);
+    }
+}
