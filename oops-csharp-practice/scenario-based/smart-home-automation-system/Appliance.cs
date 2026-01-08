a61a456b9bfe7216cdbbd455b9089c1017aad104
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2026-01-08 16:38:24 +0530

    [Aniket] Add. Smart home system using interface and polymorphism with method to turn on and off appliances

---
+++ oops-csharp-practice/scenario-based/smart-home-automation-system/Appliance.cs
@@ -0,0 +1,14 @@
+﻿public abstract class Appliance : IControllable
+{
+    protected string name;
+    protected bool isOn;
+
+    public Appliance(string name)
+    {
+        this.name = name;
+        isOn = false;
+    }
+
+    public abstract void TurnOn();
+    public abstract void TurnOff();
+}
