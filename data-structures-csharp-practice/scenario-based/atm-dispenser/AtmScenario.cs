b077eaa0f22f547135c60729c9938bab136aa0fa
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2026-01-10 00:56:22 +0530

    [Aniket] Add. Atm dispenser logic with scenario-based note availability and fallback handling

---
+++ data-structures-csharp-practice/scenario-based/atm-dispenser/AtmScenario.cs
@@ -0,0 +1,18 @@
+using System;
+
+class AtmScenario
+{
+    public static void RunScenarioA(int amount)
+    {
+        Console.WriteLine("\nScenario A: All notes available");
+        int[] notesA = { 1, 2, 5, 10, 20, 50, 100, 200, 500 };
+        ATMDispenser.DispenseCash(amount, notesA);
+    }
+
+    public static void RunScenarioB(int amount)
+    {
+        Console.WriteLine("\nScenario B: ₹500 note unavailable");
+        int[] notesB = { 1, 2, 5, 10, 20, 50, 100, 200 };
+        ATMDispenser.DispenseCash(amount, notesB);
+    }
+}
