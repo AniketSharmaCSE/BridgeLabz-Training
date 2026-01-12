f2e9f5b661f958b7533a566ccea18c8e1b263680
AniketSharmaCSE <aniket.sharma_cs22@gla.ac.in>
2026-01-12 12:21:19 +0530

    [Aniket] Add. UC1 implemented to maximize revenue from optimized cutting of a rod of given length using dp

---
+++ data-structures-csharp-practice/scenario-based/metal-pipe-cutting/CostOptimization.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class CostOptimization
+{
+    static void Main()
+    {
+        int rodLength = 8; //UC1: Given rod length 8
+
+        int[] prices = { 0, 1, 4, 7, 3, 10, 10, 15, 20 };
+
+        //Create Rod object
+        Rod rod = new Rod(rodLength, prices);
+
+        //Create service object
+        RodCutting cuttingService = new RodCutting();
+
+        //UC1: Optimized revenue using DP
+        int maxRevenue = cuttingService.CalculateMaxRevenue(rod);
+        Console.WriteLine("Rod Length: " + rodLength);
+        Console.WriteLine("Maximum revenue using optimized cutting: " + maxRevenue);
+    }
+}
